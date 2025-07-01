5ac85a2bc78fbbc8d81ae7a7e1b8c2388a48dc0f
James Orson <jamesaorson@gmail.com>
2025-07-01 15:50:28 -0700

    project4: Fix last issue
    Signed-off-by: James Orson <jamesaorson@gmail.com>

--- BGPM/bgpm.py
+++ BGPM/bgpm.py
@@ -181,7 +181,7 @@ def shortest_path_by_origin_by_snapshot(cache_files):
     return shortest_path_by_origin_by_snapshot
 
 
-def _event_durations(
+def _calculate_event_durations(
     elem: pybgpstream.BGPElem,
     timestamps: dict[str, dict[str, float]],
     event_durations: dict[str, dict[str, list[float]]],
@@ -215,13 +215,12 @@ def _event_durations(
             if prefix not in timestamps[peer_ip]:
                 return
             duration = timestamp - timestamps[peer_ip][prefix]
-            if duration == 0.0:
-                return
-            if peer_ip not in event_durations:
-                event_durations[peer_ip] = {}
-            if prefix not in event_durations[peer_ip]:
-                event_durations[peer_ip][prefix] = []
-            event_durations[peer_ip][prefix].append(duration)
+            if duration > 0.0:
+                if peer_ip not in event_durations:
+                    event_durations[peer_ip] = {}
+                if prefix not in event_durations[peer_ip]:
+                    event_durations[peer_ip][prefix] = []
+                event_durations[peer_ip][prefix].append(duration)
             del timestamps[peer_ip][prefix]
 
 
@@ -251,31 +250,11 @@ def aw_event_durations(cache_files):
 
         # implement your solution here
         for elem in stream:
-            event_type = elem.type
-            timestamp = elem.record.time
-            peer_ip = elem.peer_address
-            prefix = elem.fields.get("prefix")
-            if not prefix or not peer_ip:
-                continue
-            match event_type:
-                case "A":
-                    if peer_ip not in timestamps:
-                        timestamps[peer_ip] = {}
-                    timestamps[peer_ip][prefix] = timestamp
-                case "W":
-                    if peer_ip not in timestamps:
-                        continue
-                    if prefix not in timestamps[peer_ip]:
-                        continue
-                    duration = timestamp - timestamps[peer_ip][prefix]
-                    if duration == 0.0:
-                        continue
-                    if peer_ip not in aw_event_durations:
-                        aw_event_durations[peer_ip] = {}
-                    if prefix not in aw_event_durations[peer_ip]:
-                        aw_event_durations[peer_ip][prefix] = []
-                    aw_event_durations[peer_ip][prefix].append(duration)
-                    del timestamps[peer_ip][prefix]
+            _calculate_event_durations(
+                elem,
+                timestamps,
+                aw_event_durations,
+            )
     return aw_event_durations
 
 
@@ -307,38 +286,10 @@ def rtbh_event_durations(cache_files):
 
         # implement your solution here
         for elem in stream:
-            event_type = elem.type
-            timestamp = elem.record.time
-            peer_ip = elem.peer_address
-            prefix = elem.fields.get("prefix")
-            communities = elem.fields.get("communities", [])
-            if not prefix or not peer_ip:
-                continue
-
-            match event_type:
-                case "A":
-                    is_rtbh_event = any(
-                        "666" == community.split(":")[-1] for community in communities
-                    )
-                    if is_rtbh_event:
-                        if peer_ip not in timestamps:
-                            timestamps[peer_ip] = {}
-                        timestamps[peer_ip][prefix] = timestamp
-                    else:
-                        if peer_ip in timestamps and prefix in timestamps[peer_ip]:
-                            del timestamps[peer_ip][prefix]
-                case "W":
-                    if peer_ip not in timestamps:
-                        continue
-                    if prefix not in timestamps[peer_ip]:
-                        continue
-                    duration = timestamp - timestamps[peer_ip][prefix]
-                    if duration == 0.0:
-                        continue
-                    if peer_ip not in rtbh_event_durations:
-                        rtbh_event_durations[peer_ip] = {}
-                    if prefix not in rtbh_event_durations[peer_ip]:
-                        rtbh_event_durations[peer_ip][prefix] = []
-                    rtbh_event_durations[peer_ip][prefix].append(duration)
-                    del timestamps[peer_ip][prefix]
+            _calculate_event_durations(
+                elem,
+                timestamps,
+                rtbh_event_durations,
+                do_blackholing=True,
+            )
     return rtbh_event_durations
