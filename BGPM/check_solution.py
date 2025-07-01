18f1b0edadde231b9af35fe59fd8d921282a2560
James Orson <jamesaorson@gmail.com>
2025-07-01 14:32:04 -0700

    project4: Comment out task 2
    Signed-off-by: James Orson <jamesaorson@gmail.com>

--- BGPM/check_solution.py
+++ BGPM/check_solution.py
@@ -89,7 +89,7 @@ if __name__ == "__main__":
             # (TASK_1A, unique_prefixes_by_snapshot, "rib_files"),
             # (TASK_1B, unique_ases_by_snapshot, "rib_files"),
             # (TASK_1C, top_10_ases_by_prefix_growth, "rib_files"),
-            (TASK_2, shortest_path_by_origin_by_snapshot, "rib_files"),
+            # (TASK_2, shortest_path_by_origin_by_snapshot, "rib_files"),
             (TASK_3, aw_event_durations, "update_files"),
             (TASK_4, rtbh_event_durations, "update_files_blackholing"),
         ]
